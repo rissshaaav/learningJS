3abd02a6dd45c89db5383339f0e4ab059759763d
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-08-26 11:08:42 +0530

    learn about strings

---
+++ basics/06_strings.js
@@ -0,0 +1,8 @@
+let name = "rishav";
+let greet = "hello";
+console.log(greet + ", " + name + " kumar"); // concatenation
+console.log(`${greet}, ${name} kumar`); // string interpolation
+
+
+let str = "cricket";
+console.log(str.charAt(4));
