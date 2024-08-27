11d64c4918f6f235fe8b3fb8d527e82a8d4e8fe6
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-08-27 12:43:00 +0530

    learn more about objects

--- level1/02_objects.js
+++ level1/02_objects.js
@@ -18,9 +18,12 @@ console.log(jsUser["email"]);
 
 console.log(jsUser["full name"]); // cannot use '.' method here hence it is recommended to use brackets only.
 
+let {name, age, isLoggedIn} = jsUser; // object destructuring
+console.log(name, age, isLoggedIn);
+
 // console.table([jsUser.sym, typeof jsUser.sym]);
-// it will give the output but thi is not the correct way to use with symbols.
-// to use it as symbol, do [sym]: "mykey" and jsUser[sym].
+//> it works but this is not the correct way to go around with symbols.
+//> rather, do [sym]: "mykey" and jsUser[sym].
 console.table([jsUser[sym], typeof jsUser[sym]]);
 
 jsUser.age = 10;
@@ -28,6 +31,11 @@ console.log(jsUser);
 Object.freeze(jsUser); // now object cannot be modified
 jsUser.age = 80;
 console.log(jsUser);
+
+console.log(Object.keys(jsUser)); // returns array of all keys
+console.log(Object.values(jsUser)); // returns array of all values
+console.log(Object.entries(jsUser)); // returns an array of each key-value as an array
+console.log(jsUser.hasOwnProperty("isLoggedIn")); // returns true if property is present else false
 
 let school = {
     name: "XYZ Vidya Mandir",
@@ -42,7 +50,6 @@ school.advertisement = function () {
 
 console.log(school.advertisement());
 
-
 //~ Singleton Design Pattern
 
 let tinderUser = new Object();
@@ -50,3 +57,29 @@ tinderUser.username = "user1";
 tinderUser.name = "rishav";
 tinderUser.gender = "male";
 console.log(tinderUser);
+
+let regularUser = {
+    email: "rishav@email.com",
+    fullName: {
+        userFullName: {
+            firstName: "rishav",
+            lastName: "kumar",
+        },
+    },
+};
+console.log(regularUser.fullName?.userFullName.firstName); 
+//. ? -> in case fullName is not present, it won't give any error!
+
+
+let obj1 = {1: "one", 2: "two"};
+let obj2 = {3: "three", 4: "four"};
+let finalObj1 = Object.assign({}, obj1, obj2);
+console.log(finalObj1);
+//> first paramm is target and rest are sources
+//> all sources will be added in target
+//> that's why we put a {} so...
+//> all sources get assigned to new empty object...
+//> without affecting any source objects.
+//> can also be done using spread operator
+let finalObj2 = {...obj1, ...obj2};
+console.log(finalObj2);
