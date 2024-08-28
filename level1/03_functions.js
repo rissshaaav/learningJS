be0cf902fed9aa206d151b755c17753d276002e6
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-08-28 11:34:07 +0530

    learn about functions

--- level1/03_functions.js
+++ level1/03_functions.js
@@ -7,9 +7,10 @@ const sayMyName = function () {
     console.log("a");
     console.log("v");
 };
-
 // function call
 sayMyName();
+
+//~ taking argumnets
 
 let addTwoNumber = function (num1, num2) {
     // values inside paranthesis of function declaration -> parameters
@@ -25,6 +26,8 @@ addTwoNumber = function (num1, num2) {
 result = addTwoNumber(2, 5);
 console.log(result); // 7
 
+//~ default parameter value
+
 const greet = function (name = "Birju") {
     // default value of parameter
     return `Hello, ${name}`;
@@ -32,13 +35,16 @@ const greet = function (name = "Birju") {
 console.log(greet()); // Hello, Birju
 console.log(greet("Rishav")); // Hello, Rishav
 
-// taking any number of arguments
+//~ taking any number of arguments
+
 let takeManyNumbers = function (...nums) {
     // rest operator; returns a list
     return nums;
 };
 console.log(takeManyNumbers(1, 2, 3, 4));
 console.log(takeManyNumbers(1, 2));
+
+//~ passing object as argument
 
 let user = {
     name: "rishav",
@@ -49,3 +55,33 @@ function displayUserInfo(user) {
     console.log(`User name: ${user.name}, age: ${user.age} and ${user.gender}`);
 }
 displayUserInfo(user);
+
+//~ nested function and there scopes
+
+function one() {
+    let username = "rishav";
+    function two() {
+        let website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    // cannot access 'website' outside the scope of 'two'
+    two();
+}
+one();
+
+//~ calling before declaring
+
+hello();
+function hello() {
+    console.log("you can call me before declaring");
+}
+
+//helloAgain();
+//! ReferenceError: Cannot access 'helloAgain' before initialization
+let helloAgain = function(){
+    console.log("you cannot call me before declaring");
+    
+}
+//? cannot access a function stored in a variable before declaring it,
+//? because it is the variable that is being referred before declaration.
